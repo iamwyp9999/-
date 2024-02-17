0bdb13eb21c192713c49efa2fd423784c39d0504
Wang Yu Pou <iamwyp9999e@gmail.com>
2024-02-17 15:27:54 +0800

    Day2_1

---
+++ C_to_F.py
@@ -0,0 +1,5 @@
+print ('※將攝氏溫度轉為華氏溫度')
+c_degree = input('已知攝氏溫度為:')
+c_degree = int(c_degree)
+f_degree = c_degree * 9 / 5 + 32
+print ('攝氏溫度為',c_degree,'C時','華氏溫度為',f_degree,'F')
---
+++ learn003.py
@@ -0,0 +1,11 @@
+# "==" 用來判斷是否相同(Ture or False)
+# "!=" 用來判斷是否相異(Ture or False)
+
+x = 5
+print('x = 5')
+print('x == 5', x == 5)
+print('x != 5', x != 5)
+print('x < 2', x < 2)
+print('x > 2', x > 2)
+print('x <= 2', x <= 2)
+print('x >= 2', x >= 2)
